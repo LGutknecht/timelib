f8f2ac441460af2eaaa3f268d087ce93f73d9777
LGutknecht <lukasgutknecht@web.de>
2019-02-26 10:21:24 +0100

    Updated License

--- timelib.h
+++ timelib.h
@@ -1,6 +1,5 @@
 #ifndef TIMELIB_H_INCLUDED
 #define TIMELIB_H_INCLUDED
-
 /**
 *   Initialisieren des Structs
 **/
@@ -8,8 +7,11 @@ struct Date{
     int day;
     int month;
     int year;
-    int foy;
 };
+
+/**
+*   Deklarieren der Funktionsprototypen
+**/
 int day_of_the_year(struct Date Date);
 int is_leapyear(struct Date Date);
 struct Date input_date();
@@ -17,6 +19,5 @@ int get_days_for_month(struct Date Date);
 int exist_date(struct Date Date);
 int calc_weekday(struct Date Date);
 int calc_week_of_year(struct Date Date);
-
 
 #endif // TIMELIB_H_INCLUDED
